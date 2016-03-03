8706ab356ee8b97d4ac44af7e41f0c01241ba80b
lourdes <lurdes@gmail.com>
2016-03-03 17:14:28 -0500

    lobasa 4

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>las lobas</title>
-	<link rel="stylesheet" href="">
+	<title></title>
 </head>
 <body>
-	<ul>
-		<li><h1>ERICK MAS NA </h1></li>
-		<li><h1>EDGAR MAS NA </h1></li>
-		<li><h1>RUIZ MAS NA </h1></li>
-	</ul>
+<h1>edgar</h1>
 </body>
 </html>
